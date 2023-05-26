991d9b1b821469a1fa2c22ff1b799a4302f36349
kblack-git <keithblack@gmail.com>
2023-05-26 11:45:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import react from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [dogPic, setDogPic ] = useState()
+
+  const url = '/breeds/list/all/random';
+const options = {
+	method: 'GET',
+	// headers: {
+	// 	'X-RapidAPI-Key': '7819b75dccmshf542472e7d87915p19fe7ajsn2dba90e6ab60',
+	// 	'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	// }
+};
+
+useEffect(() => {
+    const fetchDogPic = async () => {
+      try {
+	const response = await fetch(url, options);
+	const result =  response.json();
+	console.log(result.message)
+  setDogPic(result)
+        }  catch(error) {
+    console.error(error);
+
+}
+        // try {
+        //   const response = await fetch(
+        //     `https://data.sec.gov/submissions/CIK${cikString}.json`
+        //   );
+        //   const data = await response.json();
+        // //   console.log('secGovFetch', data)
+        //   setDataFromSecGov(data);
+        // //   console.log(dataFromSecGov)
+        // //   console.log(dataFromSecGov.filings.recent.accessionNumber[0])
+        // //   console.log(dataFromSecGov.filings.recent.primaryDocument[0])
+        // //   console.log(`www.sec.gov/Archives/edgar/data/`+(dataFromSecGov.cik)+'/'+(dataFromSecGov.filings.recent.accessionNumber[0])/replace(/-/g, '')+'/'+(dataFromSecGov.filings.recent.primaryDocument[0]))
+        // //   setUrl(`www.sec.gov/Archives/edgar/data/${dataFromSecGov.cik}/${(dataFromSecGov.filings.recent.accessionNumber[0]).replace(/-/g, '')}/${dataFromSecGov.filings.recent.primaryDocument[0]}`)
+        //   console.log(url)
+        //   console.log("testing", url)
+        // } catch (error) {
+        //   console.error(error);
+        
+      }
+      fetchDogPic()
+    }
+   ,[]);
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <img src={dogPic.message} />
+       
       </header>
     </div>
   );
